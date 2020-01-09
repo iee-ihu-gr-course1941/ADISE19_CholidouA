1dc55b77993275768162cb5da2a1c9d30e502aca
cholaf96 <57863377+cholaf96@users.noreply.github.com>
2020-01-09 22:23:15 +0200

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,7 +1,3 @@
 <?php
-   define('DB_SERVER', '/home/student/it144329/mysql/run/mysql.sock');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '123');
-   define('DB_DATABASE', 'triliza');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   $db = mysqli_connect('users.iee.ihu.gr/~it144329/mysql/run/mysql.sock','root','123','triliza');
 ?>
