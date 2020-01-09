db4a27a1e806da105b2502b6db49eb2241114959
cholaf96 <57863377+cholaf96@users.noreply.github.com>
2020-01-09 22:27:20 +0200

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,3 +1,8 @@
 <?php
-   $db = mysqli_connect('users.iee.ihu.gr/~it144329/mysql/run/mysql.sock','root','123','triliza');
+
+$db = new mysqli('localhost', 'root', '123', 'triliza',null,' /home/student/it/2014/it144329/mysql/run/mysql.sock');
+if ($db->connect_errno) {
+    echo "Failed to connect to MySQL: (" . 
+    $db->connect_errno . ") " . $db->connect_error;
+}
 ?>
