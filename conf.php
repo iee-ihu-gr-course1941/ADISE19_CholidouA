6d6052dc9aff9f8390f03ff1d9f38f558626539e
cholaf96 <57863377+cholaf96@users.noreply.github.com>
2020-01-09 22:12:29 +0200

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,5 +1,5 @@
 <?php
-   define('DB_SERVER', 'localhost:3306');
+   define('DB_SERVER', '~/mysql/run/mysql.sock');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', '123');
    define('DB_DATABASE', 'triliza');
