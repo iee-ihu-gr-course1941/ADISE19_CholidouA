0a32b4a2d406b83c87ba2d6bad2bd34893834cae
cholaf96 <57863377+cholaf96@users.noreply.github.com>
2020-01-09 22:28:15 +0200

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = new mysqli('localhost', 'root', '123', 'triliza',null,' /home/student/it/2014/it144329/mysql/run/mysql.sock');
+$db = new mysqli('localhost', 'root', '123', 'triliza',null,'/home/student/it/2014/it144329/mysql/run/mysql.sock');
 if ($db->connect_errno) {
     echo "Failed to connect to MySQL: (" . 
     $db->connect_errno . ") " . $db->connect_error;
